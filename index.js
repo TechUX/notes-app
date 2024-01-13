285ebc741c69e48fdbcf48c72678b329aca95508
TechUX <connect.world12345@gmail.com>
2024-01-14 03:02:42 +0530

    Notes endpoint added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+require("./config/dbConfig");
+const notes = require("./routes/notes")
 
 const PORT = process.env.PORT || 8080 ;
 
 const app = express() ;
 
-app.get("/", (_,resp) => {
-    resp.status(200).send("To kaise hai aap log") ;
+app.get("/", (_,res) => {
+    res.status(200).send("To kaise hai aap log") ;
 })
 
+app.use("/notes",notes) ;
+
+app.all("*",(_,res) => {
+    res.status(404).json({"message":"Page Not Found!"});
+})
 
 app.listen(PORT,() => {
     console.log(`Listen on Port : ${PORT}`) ;
